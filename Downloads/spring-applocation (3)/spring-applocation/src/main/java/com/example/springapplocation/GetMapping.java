23edd5773400efd4d1f24f0e3888d26b10960aef
oswinjoy <120293081+oswinjoy@users.noreply.github.com>
2023-02-03 01:51:06 +0530

    first

---
+++ Downloads/spring-applocation (3)/spring-applocation/src/main/java/com/example/springapplocation/GetMapping.java
@@ -0,0 +1,5 @@
+package com.example.springapplocation;
+
+public @interface GetMapping {
+
+}
---
+++ Downloads/spring-applocation (3)/spring-applocation/src/main/java/com/example/springapplocation/SpringApplocationApplication.java
@@ -0,0 +1,16 @@
+package com.example.springapplocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApplocationApplication.class, args);
+		
+		System.out.println("Hi this is oswinjoy from devops");
+	
+	}
+
+}
